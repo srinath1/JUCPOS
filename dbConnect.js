004d37afc062250ab515b3a67f23b89f5f78c204
Srinath N.S <srinath1@users.noreply.github.com>
2022-05-14 15:19:37 +0200

    Update dbConnect.js

--- dbConnect.js
+++ dbConnect.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-const URL="mongodb+srv://nasa:nasa@graphqlnode.z5vfw.mongodb.net/pointofsale?retryWrites=true&w=majority"
+const URL="mongodb+srv://jucpos@graphqlnode.z5vfw.mongodb.net/pointofsale?retryWrites=true&w=majority"
 mongoose.connect(URL)
 let connectionobj=mongoose.connection;
 connectionobj.on('connected',()=>{
